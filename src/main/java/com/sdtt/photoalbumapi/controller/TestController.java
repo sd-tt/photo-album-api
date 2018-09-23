fb809225c861c1c47e1cd5b0036b3609ffe2446e
Tomasz Truszkowski <tomasz.truszkowski2013@gmail.com>
2018-09-23 23:02:18 +0200

    keycloak integration

---
+++ src/main/java/com/sdtt/photoalbumapi/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.sdtt.photoalbumapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class TestController {
+
+    @GetMapping("test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Success!");
+    }
+}
